90561f42b14050c447d864447efff6e7318202d1
Ozz-Matheus <montesinos.quintana@gmail.com>
2025-04-20 22:52:09 -0600

    Por sugerencia de Paty el botón de descarga paso a ser verde.

--- app/Models/File.php
+++ app/Models/File.php
@@ -72,6 +72,18 @@ class File extends Model
         $url = $this->getDownloadUrl();
         $filename = $this->title;
 
-        return "<a href='{$url}' download='{$filename}' class='inline-flex items-center px-3 py-1.5 bg-primary-600 text-white text-sm font-medium rounded-md hover:bg-primary-700 transition'>Download</a>";
+        return <<<HTML
+    <a href="{$url}" download="{$filename}"
+        style="--c-400:var(--success-400);--c-500:var(--success-500);--c-600:var(--success-600);"
+        class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75
+               focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-success fi-color-success fi-size-md fi-btn-size-md
+               gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm
+               bg-custom-600 text-white hover:bg-custom-500
+               focus-visible:ring-custom-500/50
+               dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50 fi-ac-action fi-ac-btn-action">
+
+        <span class="fi-btn-label">Download</span>
+    </a>
+    HTML;
     }
 }
